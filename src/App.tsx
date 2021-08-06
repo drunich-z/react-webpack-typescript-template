488e0b209eb2e79fea423de0764260bfe4c3393e
drunich-z <zverev-and@yandex.ru>
2021-08-06 17:52:20 +0700

    webpack multi environment (dev-prod)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import REACT_LOGO_SVG from './react-logo.svg';
 
 export const App = () => {
     return <>
-        <h1>React TypeScript Webpack Starter Template</h1>
+        <h1>React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
         <img className="img-png" src={REACT_LOGO_PNG} alt="react logo png" />
         <img className="img-svg" src={REACT_LOGO_SVG} alt="react logo png" />
     </>
