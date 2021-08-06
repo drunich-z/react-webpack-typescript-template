29b0fffb391c5922f06163055f6259fb8bcfb90b
drunich-z <zverev-and@yandex.ru>
2021-08-06 17:25:51 +0700

    end use pict

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './styles.css';
 import REACT_LOGO_PNG from './react-logo.png';
-
+import REACT_LOGO_SVG from './react-logo.svg';
 
 export const App = () => {
-    return <h1>React TypeScript Webpack Starter Template</h1>
+    return <>
+        <h1>React TypeScript Webpack Starter Template</h1>
+        <img className="img-png" src={REACT_LOGO_PNG} alt="react logo png" />
+        <img className="img-svg" src={REACT_LOGO_SVG} alt="react logo png" />
+    </>
 }
